e79f27c181bb652adfcbce4418d9c50343bfa218
Hugo Lima Romão <hugo8romao@gmail.com>
2020-12-22 17:22:57 -0400

    adicionando bombas

--- Fazendo um jogo com Phaser3/main.js
+++ Fazendo um jogo com Phaser3/main.js
@@ -21,6 +21,7 @@ let platforms;
 let cursors;
 let score = 0;
 let scoreText;
+let bombs;
 
 var game = new Phaser.Game(config);
 
@@ -91,12 +92,36 @@ function create() {
 
     score += 35;
     scoreText.setText(`Score: ${score}`);
+
+    if (stars.countActive(true) === 0) {
+      stars.children.iterate(function (child) {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+
+      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+      var bomb = bombs.create(x, 16, 'bomb');
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
   }
 
   this.physics.add.collider(stars, platforms);
   this.physics.add.overlap(player, stars, collectStar, null, this);
 
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'});
+  // Criando as bombas
+  function hitBomb(player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    gameOver = true;
+  }
+
+  bombs = this.physics.add.group();
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update() {

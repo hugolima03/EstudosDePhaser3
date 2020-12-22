e25c885233016047063a7e429e09a6b2f7e1e1e1
Hugo Lima Romão <hugo8romao@gmail.com>
2020-12-22 16:48:19 -0400

    adicionando movimentação e aplicando animações criadas

--- Fazendo um jogo com Phaser3/main.js
+++ Fazendo um jogo com Phaser3/main.js
@@ -3,11 +3,11 @@ var config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       gravity: { y: 300 },
       debug: false,
-    }
+    },
   },
   scene: {
     preload: preload,
@@ -16,59 +16,77 @@ var config = {
   },
 };
 
+let player;
+let platforms;
+let cursors;
+
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude',
-    'assets/dude.png',
-    {frameWidth: 32, frameHeight: 48}
-    );
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
 }
 
-
 function create() {
-  let platforms;
   // Criando plataforma e o background
 
-  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, "sky");
 
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
   // Criando o player
-  
-  let player = this.physics.add.sprite(100, 450, 'dude');
+
+  player = this.physics.add.sprite(100, 450, "dude");
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
 
   this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4} ],
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
   });
 
   this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8}),
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1,
   });
   player.body.setGravityY(300);
   this.physics.add.collider(player, platforms);
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(+160);
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-470);
+  }
+}

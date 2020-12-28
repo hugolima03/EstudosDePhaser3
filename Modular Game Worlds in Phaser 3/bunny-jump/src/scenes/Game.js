0f0a78b2d1ef29b276d1e885188b509fd6960837
Hugo Lima Romão <hugo8romao@gmail.com>
2020-12-28 19:05:37 -0400

    setting up the game and creating platforms

---
+++ Modular Game Worlds in Phaser 3/bunny-jump/src/scenes/Game.js
@@ -0,0 +1,28 @@
+import Phaser from "../lib/phaser.js";
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+  preload() {
+    this.load.image('background', 'assets/Background/bg_layer1.png');
+    this.load.image('platform', 'assets/Environment/ground_grass.png');
+  }
+
+  create() {
+    this.add.image(240,320, 'background');
+    
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0;i < 5;i++) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i
+
+      const platform = platforms.create(x, y, 'platform');
+      platform.scale = 0.5;
+
+      const body = platform.body
+      body.updateFromGameObject();
+    }
+  }
+}

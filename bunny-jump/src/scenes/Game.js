5f1f8a8a625a8d3b073b3658e67e26e848f7bca5
Hugo Lima Romão <hugo8romao@gmail.com>
2021-01-02 14:55:34 -0400

    aplicando algoritmo de horizontal wrap

--- bunny-jump/src/scenes/Game.js
+++ bunny-jump/src/scenes/Game.js
@@ -10,39 +10,42 @@ export default class Game extends Phaser.Scene {
     super("game");
   }
   preload() {
-    this.load.image('background', 'assets/Background/bg_layer1.png');
-    this.load.image('platform', 'assets/Environment/ground_grass.png');
-    this.load.image('bunny-stand', 'assets/Players/bunny1_stand.png');
+    this.load.image("background", "assets/Background/bg_layer1.png");
+    this.load.image("platform", "assets/Environment/ground_grass.png");
+    this.load.image("bunny-stand", "assets/Players/bunny1_stand.png");
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   create() {
-    this.add.image(240,320, 'background').setScrollFactor(1, 0);
-    
+    this.add.image(240, 320, "background").setScrollFactor(1, 0);
+
     this.platforms = this.physics.add.staticGroup();
 
-    for (let i = 0;i < 5;i++) {
+    for (let i = 0; i < 5; i++) {
       const x = Phaser.Math.Between(80, 400);
-      const y = 150 * i
+      const y = 150 * i;
 
       /** @type {Phaser.Physics.Arcade.Sprite} */
-      const platform = this.platforms.create(x, y, 'platform');
+      const platform = this.platforms.create(x, y, "platform");
       platform.scale = 0.5;
 
       /** @type {Phaser.Physics.Arcade.StaticBody} */
-      const body = platform.body
+      const body = platform.body;
       body.updateFromGameObject();
     }
 
-    this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+    this.player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
     this.physics.add.collider(this.platforms, this.player);
 
-    this.player.body.checkCollision.up = false
-    this.player.body.checkCollision.left = false
-    this.player.body.checkCollision.right = false
+    this.player.body.checkCollision.up = false;
+    this.player.body.checkCollision.left = false;
+    this.player.body.checkCollision.right = false;
 
     this.cameras.main.startFollow(this.player);
+    this.cameras.main.setDeadzone(this.scale.width * 1.5);
   }
 
   update(t, dt) {
@@ -50,18 +53,18 @@ export default class Game extends Phaser.Scene {
       /** @type {Phaser.Physics.Arcade.Sprite} */
       const platform = child;
 
-      const  scrollY = this.cameras.main.scrollY;
-      
+      const scrollY = this.cameras.main.scrollY;
+
       if (platform.y >= scrollY + 700) {
         platform.y = scrollY - Phaser.Math.Between(50, 100);
         platform.body.updateFromGameObject();
       }
-    })
+    });
 
     const touchingDown = this.player.body.touching.down;
 
     if (touchingDown) {
-      this.player.setVelocityY(-300)
+      this.player.setVelocityY(-300);
     }
 
     if (this.cursors.left.isDown && !touchingDown) {
@@ -70,6 +73,20 @@ export default class Game extends Phaser.Scene {
       this.player.setVelocityX(200);
     } else {
       this.player.setVelocityX(0);
+    }
+    this.horizontalWrap(this.player);
+
+    this.horizontalWrap(this.player);
+  }
+  /** @param {Phaser.GameObjects.Sprite} sprite*/
+
+  horizontalWrap(sprite) {
+    const halfWidth = sprite.displayWidth * 0.5;
+    const gameWidth = this.scale.width;
+    if (sprite.x < -halfWidth) {
+      sprite.x = gameWidth + halfWidth;
+    } else if (sprite.x > gameWidth + halfWidth) {
+      sprite.x = -halfWidth;
     }
   }
 }
